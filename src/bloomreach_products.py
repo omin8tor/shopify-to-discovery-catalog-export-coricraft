0d6e0a8899487697307e1fe6113a551ac8dc1844
Codebeans <121491705+codebeans01@users.noreply.github.com>
2025-08-14 20:29:37 +0530

    Update bloomreach_products.py

--- src/bloomreach_products.py
+++ src/bloomreach_products.py
@@ -46,23 +46,21 @@ def apply_mappings(attributes, mappings):
                 
 
 def create_product(product, shopify_url):
+    out_product = {
+        "id": product["id"],
+        "attributes": product["attributes"].copy(),
+        "variants": {}
+    }
 
-  out_product = {
-    "id": product["id"],
-    "attributes": product["attributes"].copy(),
-    "variants": {}
-  }
+    # container for input product attributes
+    in_pa = product["attributes"]
 
-  # container for input product attributes
-  in_pa = product["attributes"]
-  
-  # container for the transformed product attributes and variants
-  # these dictionaries will be merged into out_product at the end
-  out_pa = out_product["attributes"]
+    # container for the transformed product attributes
+    out_pa = out_product["attributes"]
 
-  out_pa["url"] = f"https://{shopify_url}/products/" + in_pa["sp.handle"]
+    out_pa["url"] = f"https://{shopify_url}/products/" + in_pa["sp.handle"]
 
-
+    # Process labels from two possible keys
     labels_list = []
     for key in ["spvm.custom.additional_label", "spvm.custom.product_labels_values"]:
         val = in_pa.get(key)
@@ -72,71 +70,150 @@ def create_product(product, shopify_url):
             labels_list.extend(v for v in val if v)
 
     if labels_list:
-        # remove duplicates while preserving order
+        # Remove duplicates while preserving order
         seen = set()
         labels_list = [x for x in labels_list if not (x in seen or seen.add(x))]
         out_pa["labels_new"] = labels_list
 
+    # Set thumb_image from featured image
+    if "sp.featuredImage" in in_pa and in_pa["sp.featuredImage"] and "url" in in_pa["sp.featuredImage"]:
+        out_pa["thumb_image"] = in_pa["sp.featuredImage"]["url"]
+
+    apply_mappings(out_pa, PRODUCT_MAPPINGS)
+
+    # Iterate over each variant
+    for v_id, variant in product["variants"].items():
+        in_va = variant["attributes"]
+        out_va = in_va.copy()
+        out_product["variants"][v_id] = {}
+
+        # Handle pricing
+        if "sv.compareAtPrice" in in_va and in_va["sv.compareAtPrice"]:
+            if in_va["sv.compareAtPrice"] == in_va["sv.price"]:
+                out_va["price"] = in_va["sv.compareAtPrice"]
+            else:
+                out_va["price"] = in_va["sv.compareAtPrice"]
+                out_va["sale_price"] = in_va["sv.price"]
+        else:
+            out_va["price"] = in_va["sv.price"]
+
+        # Extract color and size from selectedOptions
+        if "sv.selectedOptions" in in_va and in_va["sv.selectedOptions"]:
+            for option in in_va["sv.selectedOptions"]:
+                if option.get("name") and "Color" in option["name"]:
+                    out_va["color"] = option["value"]
+                if option.get("name") and "Size" in option["name"]:
+                    out_va["size"] = option["value"]
+
+        # Set availability
+        if "sv.availableForSale" in in_va and in_va["sv.availableForSale"]:
+            out_va["availability"] = True
+        else:
+            out_va["availability"] = True  # ← You might want this to be False instead
+            out_va["sv.availableForSale"] = False
+
+        # Set variant thumb image
+        if "sv.image" in in_va and in_va["sv.image"] and "url" in in_va["sv.image"]:
+            out_va["thumb_image"] = in_va["sv.image"]["url"]
+
+        out_product["variants"][v_id]["attributes"] = out_va
+
+    return out_product
+
+
+
+# def create_product(product, shopify_url):
+
+#   out_product = {
+#     "id": product["id"],
+#     "attributes": product["attributes"].copy(),
+#     "variants": {}
+#   }
+
+#   # container for input product attributes
+#   in_pa = product["attributes"]
+  
+#   # container for the transformed product attributes and variants
+#   # these dictionaries will be merged into out_product at the end
+#   out_pa = out_product["attributes"]
+
+#   out_pa["url"] = f"https://{shopify_url}/products/" + in_pa["sp.handle"]
+
+
+#    labels_list = []
+#     for key in ["spvm.custom.additional_label", "spvm.custom.product_labels_values"]:
+#         val = in_pa.get(key)
+#         if isinstance(val, str):
+#             labels_list.extend(v.strip() for v in val.split(",") if v.strip())
+#         elif isinstance(val, list):
+#             labels_list.extend(v for v in val if v)
+
+#     if labels_list:
+#         # remove duplicates while preserving order
+#         seen = set()
+#         labels_list = [x for x in labels_list if not (x in seen or seen.add(x))]
+#         out_pa["labels_new"] = labels_list
+
 
    
 
-  # if in_pa["sp.status"] == "ACTIVE" and "sp.totalInventory" in in_pa and in_pa["sp.totalInventory"] > 0:
-  #   out_pa["availability"] = True
-  # else:
-  #   out_pa["availability"] = False
+#   # if in_pa["sp.status"] == "ACTIVE" and "sp.totalInventory" in in_pa and in_pa["sp.totalInventory"] > 0:
+#   #   out_pa["availability"] = True
+#   # else:
+#   #   out_pa["availability"] = False
 
-  # set thumb_image from featured image (alternatively, this could be large_image)
-  # https://shopify.dev/api/admin-graphql/2023-01/objects/product#field-product-featuredimage
-  if "sp.featuredImage" in in_pa and in_pa["sp.featuredImage"] and "url" in in_pa["sp.featuredImage"]:
-      out_pa["thumb_image"] = in_pa["sp.featuredImage"]["url"]
+#   # set thumb_image from featured image (alternatively, this could be large_image)
+#   # https://shopify.dev/api/admin-graphql/2023-01/objects/product#field-product-featuredimage
+#   if "sp.featuredImage" in in_pa and in_pa["sp.featuredImage"] and "url" in in_pa["sp.featuredImage"]:
+#       out_pa["thumb_image"] = in_pa["sp.featuredImage"]["url"]
 
-  apply_mappings(out_pa, PRODUCT_MAPPINGS)
+#   apply_mappings(out_pa, PRODUCT_MAPPINGS)
 
-  # iterate over each variant
-  for v_id, variant in product["variants"].items():
+#   # iterate over each variant
+#   for v_id, variant in product["variants"].items():
 
-    # container of input and output variant attributes
-    in_va = variant["attributes"]
-    out_va = variant["attributes"].copy()
-    out_product["variants"][v_id] = {}
+#     # container of input and output variant attributes
+#     in_va = variant["attributes"]
+#     out_va = variant["attributes"].copy()
+#     out_product["variants"][v_id] = {}
 
-    if "sv.compareAtPrice" in in_va and in_va["sv.compareAtPrice"]:
-      if in_va["sv.compareAtPrice"] == in_va["sv.price"]:
-        out_va["price"] = in_va["sv.compareAtPrice"]
-      else:
-        out_va["price"] = in_va["sv.compareAtPrice"]
-        out_va["sale_price"] = in_va["sv.price"]
-    else:
-      out_va["price"] = in_va["sv.price"]
+#     if "sv.compareAtPrice" in in_va and in_va["sv.compareAtPrice"]:
+#       if in_va["sv.compareAtPrice"] == in_va["sv.price"]:
+#         out_va["price"] = in_va["sv.compareAtPrice"]
+#       else:
+#         out_va["price"] = in_va["sv.compareAtPrice"]
+#         out_va["sale_price"] = in_va["sv.price"]
+#     else:
+#       out_va["price"] = in_va["sv.price"]
 
-    # set color, size from selectedOptions
-    # TODO: set other options to custom attributes
-    if "sv.selectedOptions" in in_va:
-      if in_va["sv.selectedOptions"] and len(in_va["sv.selectedOptions"]) > 0:
-        for option in in_va["sv.selectedOptions"]:
-          if "name" in option and "value" in option and "Color" in option[
-              "name"]:
-            out_va["color"] = option["value"]
-            # br_va["variants_color"] = option["value"]
-          if "name" in option and "value" in option and "Size" in option[
-              "name"]:
-            out_va["size"] = option["value"]
+#     # set color, size from selectedOptions
+#     # TODO: set other options to custom attributes
+#     if "sv.selectedOptions" in in_va:
+#       if in_va["sv.selectedOptions"] and len(in_va["sv.selectedOptions"]) > 0:
+#         for option in in_va["sv.selectedOptions"]:
+#           if "name" in option and "value" in option and "Color" in option[
+#               "name"]:
+#             out_va["color"] = option["value"]
+#             # br_va["variants_color"] = option["value"]
+#           if "name" in option and "value" in option and "Size" in option[
+#               "name"]:
+#             out_va["size"] = option["value"]
 
-    out_va["availability"] = False
-    if "sv.availableForSale" in in_va and in_va["sv.availableForSale"]:
-      out_va["availability"] = True
-    else:
-      out_va["availability"] = True
-      out_va["sv.availableForSale"] = False
+#     out_va["availability"] = False
+#     if "sv.availableForSale" in in_va and in_va["sv.availableForSale"]:
+#       out_va["availability"] = True
+#     else:
+#       out_va["availability"] = True
+#       out_va["sv.availableForSale"] = False
 
-    # set thumb_image (swatch_image isn't a standard shopify concept as far as I can tell)
-    # https://shopify.dev/api/admin-graphql/2023-01/objects/ProductVariant#field-productvariant-image
-    if "sv.image" in in_va and in_va["sv.image"] and "url" in in_va["sv.image"]:
-      out_va["thumb_image"] = in_va["sv.image"]["url"]
+#     # set thumb_image (swatch_image isn't a standard shopify concept as far as I can tell)
+#     # https://shopify.dev/api/admin-graphql/2023-01/objects/ProductVariant#field-productvariant-image
+#     if "sv.image" in in_va and in_va["sv.image"] and "url" in in_va["sv.image"]:
+#       out_va["thumb_image"] = in_va["sv.image"]["url"]
     
-    out_product["variants"][v_id]["attributes"] = out_va
+#     out_product["variants"][v_id]["attributes"] = out_va
 
-  return out_product
+#   return out_product
 
 
 def main(fp_in, fp_out, shopify_url):
